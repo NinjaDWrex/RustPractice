47583594e14fed27c1699423dd2bc8d47a7cff89
ewfwefewf eworhewhoroh <kerem.yaprak555@gmail.com>
2024-03-25 07:26:13 +0000

    done in english, basic learning variables and functions etc

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    let string : String = rizz();
+    println!("{string}");
+}
+fn rizz() -> String {
+    let x : String = "Ohio Gyatt".to_string();
+    x
+}
